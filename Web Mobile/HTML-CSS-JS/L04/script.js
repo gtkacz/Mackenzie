960a81dde72ee861e0c7d3b2e576c636e445ea25
gtkacz <gabriel.tkacz@scandiweb.com>
2022-09-03 22:17:19 -0300

    att

--- Web Mobile/HTML-CSS-JS/L04/script.js
+++ Web Mobile/HTML-CSS-JS/L04/script.js
@@ -1,19 +1,42 @@
 const url = "https://mack-webmobile.vercel.app/api/users";
 
-async function buscarEmpregados() {
+async function getEmployees() {
     const response = await fetch(url);
     const employees = await response.json();
     console.log(employees[0])
 
+    createCardRows(employees);
+}
+
+function createCardRows(employees) {
+    // let row_count = Math.ceil(employees.length / 3);
+    let row_count = 0;
+
     let html = "";
+
+    let html_row = '<div class="card-row">';
+
     for (let i = 0; i < employees.length; i++) {
-        html += createNewCard(employees[i]);
+        html_row += createNewCard(employees[i]);
+        row_count++;
+
+        if (row_count == 3) {
+            html += (html_row + "</div>" + "<br>");
+            html_row = '<div class="card-row">';
+            row_count = 0;
+        }
     }
+
+    if (!html.endsWith("</div></div>")) {
+        html += (html_row + "</div>");
+    }
+
     document.getElementById("container").innerHTML = html;
 }
 
 function createNewCard(employee) {
     let html = `<div class="card" id="${employee._id}">`;
+
 
     html += `<header class="card-header"><img src="${employee.avatar}" class="avatar"></header>`;
 
@@ -25,7 +48,7 @@ function createNewCard(employee) {
 
     html += `<section class="content-grid">`;
 
-    html += `<div class="content-line"><span>Salário: <b>R$ ${employee.salary}</b></span><img src="images/edit.png" class="icon"></div>`;
+    html += `<div class="content-line"><span>Salário: <b>R$ ${thousandSeparator(employee.salary)}</b></span><img src="images/edit.png" class="icon"></div>`;
 
     html += `<div class="content-line"><span>Aniversário: <b>${employee.date}</b></span><img src="images/delete.png" class="icon"></div>`;
 
@@ -39,4 +62,8 @@ function createNewCard(employee) {
     return html;
 }
 
-buscarEmpregados();
+function thousandSeparator(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") + ",00";
+}
+
+getEmployees();
