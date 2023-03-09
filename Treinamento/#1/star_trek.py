b097844a543b4cc1f3085de4f5beaf32754ac526
Gabriel Tkacz <gabriel.tkacz@gscap.com.br>
2023-03-09 14:30:09 -0300

    Bugfix on star_trek.py

--- Treinamento/#1/star_trek.py
+++ Treinamento/#1/star_trek.py
@@ -2,32 +2,26 @@ def main():
     shape = int(input())
 
     raw_content = input().split()
-        
+
     content = [int(x) for x in raw_content]
 
     initial_content = tuple(content)
 
-    def is_even(number):
-        return number % 2 == 0
-        
     curr_star = 0
 
     while True:
-        try:
-            if is_even(content[curr_star]):
-                future_star = curr_star-1
-            else:
-                future_star = curr_star+1
+        if (content[curr_star] % 2 == 0):
+            future_star = curr_star-1
 
-            content[curr_star] -= 1 if (content[curr_star] >= 0) else 0
+        else:
+            future_star = curr_star+1
 
-            curr_star = future_star
+        content[curr_star] -= 1 if (content[curr_star] > 0) else 0
 
-            if future_star < 0:
-                break
-        
-        except IndexError:
+        if (future_star < 0) or (future_star >= len(content)):
             break
+
+        curr_star = future_star
 
     star_sum = len([star_index+1 for star_index in range(len(content)) if (content[star_index] != initial_content[star_index])])
     sheep_sum = sum(content)
